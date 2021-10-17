9e2114f8bc7fd54adc2e83dca44c15a79142d658
Peng Chen <u6460012@anu.edu.au>
2021-10-17 15:26:32 +0800

    11111

--- src/main/java/com/example/mockband/MockBandApplication.java
+++ src/main/java/com/example/mockband/MockBandApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MockBandApplication {
-
+//111111
     public static void main(String[] args) {
         SpringApplication.run(MockBandApplication.class, args);
     }
