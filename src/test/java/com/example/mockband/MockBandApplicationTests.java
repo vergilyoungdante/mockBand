2fb9fe119e3b979ff848668d4ad7fd3dfb0a150e
vergil <yjb387759719@outlook.com>
2021-10-17 14:09:19 +0800

    Initial commit

---
+++ src/test/java/com/example/mockband/MockBandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockBandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
