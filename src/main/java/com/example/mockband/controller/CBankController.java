337c4dbf20bc104d3acd0263ad51c07ed85cbbbf
vergil <yjb387759719@outlook.com>
2021-11-01 22:40:31 +0800

    央行财务情况、货币调整初稿

---
+++ src/main/java/com/example/mockband/controller/CBankController.java
@@ -0,0 +1,21 @@
+package com.example.mockband.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/cbank")
+public class CBankController {
+
+    @RequestMapping("/total")
+    public ModelAndView totalCount(){
+        ModelAndView modelAndView = new ModelAndView("/cbank/total-count");
+        //TODO:获得所有中央银行数据
+        //
+        //我这里把数据加进去给前端用
+        //modelAndView.addObject();
+
+        return modelAndView;
+    }
+}
