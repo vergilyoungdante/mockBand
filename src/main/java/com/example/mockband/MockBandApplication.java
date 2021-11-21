6d475272b7606cf411bfef296255bf9fb073f4f4
ChangZhehao <1996jo0609hn>
2021-11-22 01:58:23 +0800

    jar to war to fit tomcat

--- src/main/java/com/example/mockband/MockBandApplication.java
+++ src/main/java/com/example/mockband/MockBandApplication.java
@@ -3,13 +3,20 @@ package com.example.mockband;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.mockband.mapper")
-public class MockBandApplication {
+public class MockBandApplication extends SpringBootServletInitializer {
 //111111
     public static void main(String[] args) {
         SpringApplication.run(MockBandApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MockBandApplication.class);
     }
 
 }
